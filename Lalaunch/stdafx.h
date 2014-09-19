b67932de03ba68bb55583cfdf2322cfedffb1ece
usagiuke <usagiuke@gmail.com>
2014-09-19 17:28:26 -0400

    GPL 3.0

--- Lalaunch/stdafx.h
+++ Lalaunch/stdafx.h
@@ -1,7 +1,19 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
+/*
+	Copyright (C) 2014  DevNull:ffevo
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
@@ -11,5 +23,3 @@
 #include <tchar.h>
 
 
-
-// TODO: reference additional headers your program requires here
