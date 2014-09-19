d42c4d43c9e54c698c6ca010fab22b6415ac7ec8
usagiuke <usagiuke@gmail.com>
2014-09-18 21:50:10 -0400

    :rabbit:
    Added command handler

--- FCBondage/FCBondage.cpp
+++ FCBondage/FCBondage.cpp
@@ -2,15 +2,18 @@
 #include "FCBondage.h"
 #include "SigScan.h"
 #include <iostream>
+#include <future>
+#include <sstream>
 #include <detours.h>
 #pragma comment( lib, "detours.lib" )
+
+
 
 BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID)
 {
 	switch (fdwReason)
 	{
 	case DLL_PROCESS_ATTACH:
-		SpawnConsole();
 		InitHook();
 		break;
 	default:
@@ -21,9 +24,22 @@ BOOL WINAPI DllMain(HINSTANCE, DWORD fdwReason, LPVOID)
 
 INT __cdecl My_ReadObtainedItemPacket(INT unk, ItemObtainedPacket* packet)
 {
-	auto NewSB = packet->SpiritBind * 0.01;
-	auto ItemName = GetItemNameFromID(packet->ItemID);
-	std::cout << ++pcount <<". Item: ~" << (ItemName != 0 ? ItemName : "Item Not Found") << "~ || New SB: " << NewSB << " %\n";
+	SetValues(packet);
+	if (AutoUpdateFlag)
+		std::async(std::launch::async, &ShowCurrentValues);
+	if (ConsoleFlag || ShowAllFlag)
+	{
+		auto NewSB = packet->SpiritBind * 0.01;
+		auto ItemName = GetItemNameFromID(packet->ItemID);
+		if (ShowAllFlag)
+		{
+			std::stringstream ss;
+			ss << "/echo ~" << (ItemName != 0 ? ItemName : "Item Not Found") << "~ || SB: " << NewSB << " %";
+			SendCommand(ss.str().c_str());
+		}
+		if (ConsoleFlag)
+			std::cout << ++pcount <<". Item: ~" << (ItemName != 0 ? ItemName : "Item Not Found") << "~ || New SB: " << NewSB << " %\n";
+	}
 	return Real_ReadObtainedItemPacket(unk,packet);
 }
 
@@ -33,18 +49,30 @@ VOID __stdcall InitHook()
 	GetModuleInformation(GetCurrentProcess(), GetModuleHandleA("ffxiv.exe"), &modinfo, sizeof(MODULEINFO));
 	SetFunctionPointer(Real_ReadObtainedItemPacket, FindPattern(ItemPacket_FuncSig,"xxxxxxx????xxxxxxx", &modinfo));
 	SetFunctionPointer(GetItemInfo, FindPattern(GetItemInfo_FuncSig,"xxxxxxx????xxx", &modinfo));
+	SetFunctionPointer(Real_IncommingCommand, FindPattern(IncommingCommand_FuncSig,"xxxxxxxxxxxxxx", &modinfo) - 0x23);
+	SetFunctionPointer(InitCommandStruct, FindPattern(InitCommandStruct_FuncSig,"xxxxxxxxxxxx", &modinfo) - 0x2D);
+	InitInstanceRetrievalRoutines(&modinfo);
+	InitThreadSecurity(&modinfo);
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
 	DetourAttach(&(PVOID&)Real_ReadObtainedItemPacket,(PVOID)My_ReadObtainedItemPacket);
+	DetourAttach(&(PVOID&)Real_IncommingCommand,(PVOID)My_IncommingCommand);
+	DetourAttach(&(PVOID&)Real_VerifyThreadOgrigin,(PVOID)My_VerifyThreadOgrigin);
 	DetourTransactionCommit();
 }
 
-VOID __stdcall SpawnConsole()
+VOID __stdcall ToggleConsole(BOOL on)
 {
-	AllocConsole();
-	freopen("CONIN$", "r", stdin);
-	freopen("CONOUT$", "w", stdout);
-	freopen("CONOUT$", "w", stderr);
+	ConsoleFlag = on;
+	if (on)
+	{
+		AllocConsole();
+		freopen("CONIN$", "r", stdin);
+		freopen("CONOUT$", "w", stdout);
+		freopen("CONOUT$", "w", stderr);
+		return;
+	}
+	FreeConsole();
 }
 
 const char* __stdcall GetItemNameFromID(USHORT ItemID)
@@ -53,4 +81,221 @@ const char* __stdcall GetItemNameFromID(USHORT ItemID)
 	if (itemInfo == NULL)
 		return NULL;
 	return &reinterpret_cast<const char*>(itemInfo)[itemInfo->OffsetOfName + offsetof(BasicItemInfo,OffsetOfName)];
+}
+
+VOID __stdcall InitInstanceRetrievalRoutines(void* modinfo)
+{
+	auto pppRootInputObject = FindPattern(Input_ObjectSig,"xxxxxxxxxxxxxxx", (MODULEINFO*)modinfo) -0x24;
+	ppRootInputObject = *(void***)pppRootInputObject;
+	int JmpDistance = *(int*)(pppRootInputObject + 5) + 9;
+	SetFunctionPointer(GetUserInputObjectFromRootInput, pppRootInputObject + JmpDistance);
+}
+
+VOID __stdcall InitThreadSecurity(void* modinfo)
+{
+	auto ppTls = FindPattern(VerifyThreadOgrigin_FuncSig,"xxxxxxx????x",(MODULEINFO*)modinfo) + 7;
+	pThreadVerifyIndex = *(PDWORD*)ppTls;
+	SetFunctionPointer(Real_VerifyThreadOgrigin, ppTls - 4);
+}
+
+BOOL __fastcall My_IncommingCommand(void* pChatLogObject, int, CommandStruct* pCmdStruct, UserInputObject* pInputObject)
+{
+	if (pCmdStruct->pCommandString[1] != 0x2F)
+		return Real_IncommingCommand(pChatLogObject,pCmdStruct,pInputObject);
+
+	auto firstArg = FirstArg(pCmdStruct->pCommandString);
+	if (StringContains(pCmdStruct->pCommandString, "//console"))
+	{
+		if (firstArg == NULL)
+			return 0;
+		if (!strcmp(firstArg,"on"))
+			ToggleConsole(TRUE);
+		else if (!strcmp(firstArg,"off"))
+			ToggleConsole(FALSE);
+		return 1;
+	}
+	if (StringContains(pCmdStruct->pCommandString, "//autoupdate"))
+	{
+		if (firstArg == NULL)
+			return 0;
+		if (!strcmp(firstArg,"on"))
+			AutoUpdateFlag = TRUE;
+		else if (!strcmp(firstArg,"off"))
+			AutoUpdateFlag = FALSE;
+		return 1;
+	}
+	if (StringContains(pCmdStruct->pCommandString, "//showall"))
+	{
+		if (firstArg == NULL)
+			return 0;
+		if (!strcmp(firstArg,"on"))
+			AutoUpdateFlag = TRUE;
+		else if (!strcmp(firstArg,"off"))
+			AutoUpdateFlag = FALSE;
+		return 1;
+	}
+	if (StringContains(pCmdStruct->pCommandString, "//showcur"))
+	{
+		std::async(std::launch::async, &ShowCurrentValues);
+		return 1;
+	}
+	return 0;
+}
+
+VOID __stdcall SendCommand(const char* pCmdStr)
+{
+	CommandStruct cmdStruct;
+	InitCommandStruct(&cmdStruct,pCmdStr,-1);
+	auto userInputObject = GetUserInputObjectFromRootInput(*ppRootInputObject);
+	auto chatLogObject = userInputObject->CalculateAddressOfChatLog[8](userInputObject);
+	Real_IncommingCommand(chatLogObject,&cmdStruct,userInputObject);
+}
+
+INT __fastcall My_VerifyThreadOgrigin(VOID* pThis, INT, INT a2, CHAR a3, INT a4)
+{
+	auto ctTls = FastTls(*pThreadVerifyIndex, NULL);
+	if (ctTls == NULL)
+		FastTls(*pThreadVerifyIndex, pThreadVerify);
+	else
+		pThreadVerify = ctTls;
+	return Real_VerifyThreadOgrigin(pThis,a2,a3,a4);
+}
+
+BOOL __stdcall StringContains(const char* str1, const char* str2)
+{
+	return strstr(str1,str2) != NULL;
+}
+
+const char* __stdcall FirstArg(const char* pCmd)
+{
+	while (pCmd[0])
+	{
+		if (pCmd++[0] == ' ')
+			return pCmd;
+	}
+	return NULL;
+}
+
+VOID __stdcall SetValues(ItemObtainedPacket* packet)
+{
+	auto NewSB = packet->SpiritBind * 0.01;
+	switch (packet->ItemID)
+	{
+	case 7863:
+		Curtana = NewSB;
+		break;
+	case 7864:
+		Sphairai = NewSB;
+		break;
+	case 7865:
+		Bravura = NewSB;
+		break;
+	case 7866:
+		GaeBolg = NewSB;
+		break;
+	case 7867:
+		ArtemisBow = NewSB;
+		break;
+	case 7868:
+		Thyrus = NewSB;
+		break;
+	case 7869:
+		StardustRod = NewSB;
+		break;
+	case 7870:
+		VeilOfWiyu = NewSB;
+		break;
+	case 7871:
+		Omnilex = NewSB;
+		break;
+	case 7872:
+		HolyShield = NewSB;
+		break;
+	}
+}
+
+VOID __stdcall ShowCurrentValues()
+{
+	const char* itemName = NULL;
+
+	if (Curtana != NULL)
+	{
+		itemName = GetItemNameFromID(7863);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << Curtana << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (Sphairai != NULL)
+	{
+		itemName = GetItemNameFromID(7864);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << Sphairai << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (Bravura != NULL)
+	{
+		itemName = GetItemNameFromID(7865);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << Bravura << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (GaeBolg != NULL)
+	{
+		itemName = GetItemNameFromID(7866);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << GaeBolg << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (ArtemisBow != NULL)
+	{
+		itemName = GetItemNameFromID(7867);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << ArtemisBow << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (Thyrus != NULL)
+	{
+		itemName = GetItemNameFromID(7868);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << Thyrus << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (StardustRod != NULL)
+	{
+		itemName = GetItemNameFromID(7869);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << StardustRod << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (VeilOfWiyu != NULL)
+	{
+		itemName = GetItemNameFromID(7870);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << VeilOfWiyu << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (Omnilex != NULL)
+	{
+		itemName = GetItemNameFromID(7871);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << Omnilex << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
+	if (HolyShield != NULL)
+	{
+		itemName = GetItemNameFromID(7872);
+		std::stringstream ss;
+		ss << "/echo ~" << (itemName != 0 ? itemName : "Item Not Found") << "~ || SB: " << HolyShield << " %";
+		Sleep(100);
+		SendCommand(ss.str().c_str());
+	}
 }
--- FCBondage/FCBondage.h
+++ FCBondage/FCBondage.h
@@ -1,4 +1,12 @@
+#include <string>
 #define SetFunctionPointer(f,p) *(PINT)(&f) = p
+
+typedef void* (__thiscall* GetChatLogObject)(void* pUserInputObject);
+
+struct UserInputObject
+{
+	GetChatLogObject* CalculateAddressOfChatLog;
+};
 
 #pragma pack(push, 1)
 struct ItemObtainedPacket
@@ -18,7 +26,38 @@ struct BasicItemInfo
 	__int32 : 32;
 	unsigned __int16 OffsetOfName;
 };
+
+struct CommandStruct
+{
+	const char* pCommandString;
+	unsigned __int32 BufferSize;
+	unsigned __int32 CommandStrlen;
+	unsigned __int32 UnkUint32;
+	unsigned __int16 UnkUint16;
+	char StringBuffer[512];
+};
+
 #pragma pack(pop)
+
+UINT8 VerifyThreadOgrigin_FuncSig[12] =
+{
+	0x5E, 0x5B, 0xC3, 0x55, 0x8B, 0xEC, 0xA1, 0x88, 0x8A, 0x4B, 0x02, 0x56
+};
+
+UINT8 Input_ObjectSig[15] =
+{
+	0x5D, 0xFF, 0xE2, 0x8B, 0x01, 0x8B, 0x50, 0x2C, 0x5D, 0xFF, 0xE2, 0x8B, 0x01, 0x8B, 0x50
+};
+
+UINT8 InitCommandStruct_FuncSig[12] =
+{
+	0x89, 0x06, 0xC6, 0x00, 0x00, 0x83, 0xFF, 0xFF, 0x75, 0x14, 0x8B, 0xC3
+};
+
+UINT8 IncommingCommand_FuncSig[14] =
+{
+	0x8D, 0x55, 0xDC, 0x52, 0x56, 0x8B, 0xCF, 0x89, 0x45, 0xEC, 0x89, 0x45, 0xF0, 0x89
+};
 
 UINT8 GetItemInfo_FuncSig[14] =
 {
@@ -30,16 +69,82 @@ UINT8 ItemPacket_FuncSig[18] =
 	0x55, 0x8B, 0xEC, 0x83, 0xEC, 0x44, 0xA1, 0x68, 0xC0, 0x11, 0x02, 0x33, 0xC5, 0x89, 0x45, 0xFC, 0x53, 0x57
 };
 
+double Curtana = NULL;
+double Sphairai = NULL;
+double Bravura = NULL;
+double GaeBolg = NULL;
+double ArtemisBow = NULL;
+double Thyrus = NULL;
+double StardustRod = NULL;
+double VeilOfWiyu = NULL;
+double Omnilex = NULL;
+double HolyShield = NULL;
+
+BOOL ShowAllFlag = FALSE;
+
+BOOL AutoUpdateFlag = TRUE;
+
+BOOL ConsoleFlag = FALSE;
+
 UINT pcount = 0;
+
+VOID** ppRootInputObject;
+
+VOID* pThreadVerify;
+
+PDWORD pThreadVerifyIndex;
 
 const char* __stdcall GetItemNameFromID(USHORT ItemID);
 
+UserInputObject* (__thiscall* GetUserInputObjectFromRootInput)(VOID* pRootInputObject);
+
+CommandStruct* (__thiscall* InitCommandStruct)(CommandStruct* pCmdStruct, const char* cmdStr, size_t strSize);
+
+BOOL (__thiscall* Real_IncommingCommand)(VOID* pChatLogObject, CommandStruct* pCmdStruct, UserInputObject* pInputObject);
+
 BasicItemInfo* (_cdecl* GetItemInfo)(USHORT);
+
+BOOL __fastcall My_IncommingCommand(VOID* pChatLogObject, INT, CommandStruct* pCmdStruct, UserInputObject* pInputObject);
+
+VOID __stdcall SendCommand(const char* pCmdStr);
 
 INT (__cdecl* Real_ReadObtainedItemPacket)(INT,ItemObtainedPacket*);
 
 INT __cdecl My_ReadObtainedItemPacket(INT unk, ItemObtainedPacket* packet);
 
+INT (__thiscall* Real_VerifyThreadOgrigin)(VOID* pThis, INT a2, CHAR a3, INT a4);
+
+INT __fastcall My_VerifyThreadOgrigin(VOID* pThis, INT, INT a2, CHAR a3, INT a4);
+
+BOOL __stdcall StringContains(const char* str1, const char* str2);
+
+const char* __stdcall FirstArg(const char* pCmd);
+
+VOID __stdcall InitInstanceRetrievalRoutines(void*);
+
+VOID __stdcall InitThreadSecurity(void*);
+
 VOID __stdcall InitHook();
 
-VOID __stdcall SpawnConsole();
+VOID __stdcall ToggleConsole();
+
+VOID __stdcall ShowCurrentValues();
+
+VOID __stdcall SetValues(ItemObtainedPacket* packet);
+
+__declspec(naked) VOID* __fastcall FastTls(DWORD dwIndex, VOID* val)
+{
+	__asm 
+	{
+		test edx,edx
+			je GET
+			lea ecx, [ecx*4+0xE10]
+		mov fs:[ecx], edx
+			xor eax,eax
+			ret
+GET:
+		lea ecx, [ecx*4+0xE10]
+		mov eax, fs:[ecx]
+		ret
+	}
+}
